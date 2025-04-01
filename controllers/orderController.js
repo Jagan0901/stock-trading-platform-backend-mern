d14b4f1271cc90ef933104cd5a3e8fececf47f06
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2025-04-01 18:43:32 +0530

    Error rectified

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,9 +1,157 @@
+// const Order = require("../models/orderModel");
+// const Portfolio = require("../models/portfolioModel");
+// const Stock = require("../models/stockModel");
+// const User = require("../models/userModel");
+// const asyncHandler = require("express-async-handler");
+// const mongoose = require("mongoose");
+
+// const placeOrder = asyncHandler(async (req, res) => {
+//   const session = await mongoose.startSession();
+//   session.startTransaction();
+
+//   try {
+//     const { stock, type, quantity } = req.body;
+//     const userId = req.user._id;
+
+//     if (!stock || !type || !quantity || quantity <= 0) {
+//       throw new Error("Invalid order details");
+//     }
+
+ 
+//     const stockData = await Stock.findOne({ symbol: stock }).session(session);
+//     if (!stockData) {
+//       throw new Error("Stock not found");
+//     }
+
+//     const price = stockData.currentPrice;
+//     const totalAmount = price * quantity;
+
+//     const user = await User.findById(userId).session(session);
+//     if (!user) {
+//       throw new Error("User not found");
+//     }
+
+//     if (type === "buy") {
+//       if (user.wallet < totalAmount) {
+//         throw new Error("Insufficient wallet balance");
+//       }
+//     } else if (type === "sell") {
+//       const portfolioItem = await Portfolio.findOne({
+//         user: userId,
+//         stock,
+//       }).session(session);
+//       if (!portfolioItem || portfolioItem.quantity < quantity) {
+//         throw new Error("Insufficient shares to sell");
+//       }
+//     }
+
+
+//     const order = await Order.create(
+//       [
+//         {
+//           user: userId,
+//           stock,
+//           type,
+//           quantity,
+//           price,
+//           status: "completed",
+//           executedAt: new Date(),
+//         },
+//       ],
+//       { session }
+//     );
+
+//     if (type === "buy") {
+//       await User.findByIdAndUpdate(
+//         userId,
+//         { $inc: { wallet: -totalAmount } },
+//         { session }
+//       );
+//     } else {
+//       await User.findByIdAndUpdate(
+//         userId,
+//         { $inc: { wallet: totalAmount } },
+//         { session }
+//       );
+//     }
+
+//     if (type === "buy") {
+//       let portfolioItem = await Portfolio.findOne({
+//         user: userId,
+//         stock,
+//       }).session(session);
+
+//       if (!portfolioItem) {
+//         portfolioItem = new Portfolio({
+//           user: userId,
+//           stock,
+//           quantity: 0,
+//           averageBuyPrice: 0,
+//         });
+//       }
+
+//       const newTotalQuantity = portfolioItem.quantity + quantity;
+//       const newAveragePrice =
+//         (portfolioItem.averageBuyPrice * portfolioItem.quantity + totalAmount) /
+//         newTotalQuantity;
+
+//       portfolioItem.quantity = newTotalQuantity;
+//       portfolioItem.averageBuyPrice = newAveragePrice;
+//       await portfolioItem.save({ session });
+//     } else {
+//       const portfolioItem = await Portfolio.findOne({
+//         user: userId,
+//         stock,
+//       }).session(session);
+//       portfolioItem.quantity -= quantity;
+
+//       if (portfolioItem.quantity === 0) {
+//         await Portfolio.findByIdAndDelete(portfolioItem._id, { session });
+//       } else {
+//         await portfolioItem.save({ session });
+//       }
+//     }
+
+//     await session.commitTransaction();
+
+
+//     const updatedUser = await User.findById(userId).select("wallet");
+
+//     res.status(201).json({
+//       success: true,
+//       data: order[0],
+//       walletBalance: updatedUser.wallet,
+//     });
+//   } catch (error) {
+//     await session.abortTransaction();
+//     res.status(400).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   } finally {
+//     session.endSession();
+//   }
+// });
+
+// const getOrders = asyncHandler(async (req, res) => {
+//   const orders = await Order.find({ user: req.user._id }).sort({
+//     createdAt: -1,
+//   });
+//   res.status(200).json({
+//     success: true,
+//     data: orders,
+//   });
+// });
+
+// module.exports = { placeOrder, getOrders };
+
+
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
 const Order = require("../models/orderModel");
 const Portfolio = require("../models/portfolioModel");
 const Stock = require("../models/stockModel");
-const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
-const mongoose = require("mongoose");
 
 const placeOrder = asyncHandler(async (req, res) => {
   const session = await mongoose.startSession();
@@ -18,7 +166,7 @@ const placeOrder = asyncHandler(async (req, res) => {
       throw new Error("Invalid order details");
     }
 
-    // Get current stock price
+    // Get stock price
     const stockData = await Stock.findOne({ symbol: stock }).session(session);
     if (!stockData) {
       throw new Error("Stock not found");
@@ -27,7 +175,7 @@ const placeOrder = asyncHandler(async (req, res) => {
     const price = stockData.currentPrice;
     const totalAmount = price * quantity;
 
-    // Check user's ability to execute order
+    // Check user's balance or holdings
     const user = await User.findById(userId).session(session);
     if (!user) {
       throw new Error("User not found");
@@ -37,18 +185,19 @@ const placeOrder = asyncHandler(async (req, res) => {
       if (user.wallet < totalAmount) {
         throw new Error("Insufficient wallet balance");
       }
-    } else if (type === "sell") {
+    } else {
       const portfolioItem = await Portfolio.findOne({
         user: userId,
         stock,
       }).session(session);
+
       if (!portfolioItem || portfolioItem.quantity < quantity) {
         throw new Error("Insufficient shares to sell");
       }
     }
 
     // Create order
-    const order = await Order.create(
+    const [order] = await Order.create(
       [
         {
           user: userId,
@@ -63,20 +212,9 @@ const placeOrder = asyncHandler(async (req, res) => {
       { session }
     );
 
-    // Update wallet without triggering password hash
-    if (type === "buy") {
-      await User.findByIdAndUpdate(
-        userId,
-        { $inc: { wallet: -totalAmount } },
-        { session }
-      );
-    } else {
-      await User.findByIdAndUpdate(
-        userId,
-        { $inc: { wallet: totalAmount } },
-        { session }
-      );
-    }
+    // Update wallet using static method (won't trigger password hash)
+    const walletUpdate = type === "buy" ? -totalAmount : totalAmount;
+    await User.updateWallet(userId, walletUpdate);
 
     // Update portfolio
     if (type === "buy") {
@@ -107,6 +245,7 @@ const placeOrder = asyncHandler(async (req, res) => {
         user: userId,
         stock,
       }).session(session);
+
       portfolioItem.quantity -= quantity;
 
       if (portfolioItem.quantity === 0) {
@@ -119,12 +258,16 @@ const placeOrder = asyncHandler(async (req, res) => {
     await session.commitTransaction();
 
     // Get fresh wallet balance
-    const updatedUser = await User.findById(userId).select("wallet");
+    const updatedUser = await User.findById(userId)
+      .select("wallet")
+      .session(session);
 
     res.status(201).json({
       success: true,
-      data: order[0],
-      walletBalance: updatedUser.wallet,
+      data: {
+        order,
+        walletBalance: updatedUser.wallet,
+      },
     });
   } catch (error) {
     await session.abortTransaction();
@@ -138,13 +281,25 @@ const placeOrder = asyncHandler(async (req, res) => {
 });
 
 const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ user: req.user._id }).sort({
-    createdAt: -1,
-  });
-  res.status(200).json({
-    success: true,
-    data: orders,
-  });
+  try {
+    const orders = await Order.find({ user: req.user._id })
+      .sort({ createdAt: -1 })
+      .populate("user", "name email");
+
+    res.status(200).json({
+      success: true,
+      count: orders.length,
+      data: orders,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
 });
 
-module.exports = { placeOrder, getOrders };
+module.exports = {
+  placeOrder,
+  getOrders,
+};
--- models/userModel.js
+++ models/userModel.js
@@ -29,49 +29,121 @@
 
 // module.exports = User;
 
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+
+// const userSchema = mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     email: { type: String, unique: true, required: true },
+//     password: { type: String, required: true },
+//     wallet: { type: Number, default: 100000 },
+//   },
+//   {
+//     timestamps: true,
+
+//     versionKey: false,
+//   }
+// );
+
+
+// userSchema.pre("save", async function (next) {
+
+//   if (!this.isModified("password")) return next();
+
+//   try {
+  
+//     if (this.password.startsWith("$2a$")) return next();
+
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+
+// userSchema.methods.needsPasswordHash = function () {
+//   return !this.password.startsWith("$2a$");
+// };
+
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// const User = mongoose.model("User", userSchema);
+
+// module.exports = User;
+
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    wallet: { type: Number, default: 100000 },
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false, // Never returned in queries
+    },
+    wallet: {
+      type: Number,
+      default: 100000,
+      min: 0,
+    },
   },
   {
     timestamps: true,
-    // Disable version key to prevent unnecessary document updates
-    versionKey: false,
+    versionKey: false, // Disable version key
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
 
-// Enhanced password hashing middleware
+// Only hash password if it was modified
 userSchema.pre("save", async function (next) {
-  // Only hash the password if it has been modified (or is new)
   if (!this.isModified("password")) return next();
 
   try {
-    // Skip hashing if already hashed
-    if (this.password.startsWith("$2a$")) return next();
-
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
-// Add method to check if password needs hashing
-userSchema.methods.needsPasswordHash = function () {
-  return !this.password.startsWith("$2a$");
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+// Static method for wallet operations
+userSchema.statics.updateWallet = async function (userId, amount) {
+  return this.findByIdAndUpdate(
+    userId,
+    { $inc: { wallet: amount } },
+    { new: true, runValidators: true }
+  ).select("wallet");
 };
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
